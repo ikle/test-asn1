460dc2240c59545ef66de1a6599c20402c0d6bb1
Alexei A. Smekalkine <ikle@ikle.ru>
2024-01-26 23:41:19 +0300

    asn1-time: make it public

--- ber-time.h
+++ ber-time.h
@@ -1,7 +1,7 @@
 /*
- * ASN.1 BER time helpers
+ * ASN.1 BER Time helpers
  *
- * Copyright (c) 2016-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ * Copyright (c) 2016-2024 Alexei A. Smekalkine <ikle@ikle.ru>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -9,7 +9,8 @@
 #ifndef _BER_TIME_H
 #define _BER_TIME_H  1
 
-#include "asn1-time.h"
+#include <asn1-mini/asn1-time.h>
+
 #include "ber-input.h"
 
 int ber_get_utctime (struct ber_input *o, struct asn1_time *t);
--- include/asn1-mini/asn1-time.h
+++ include/asn1-mini/asn1-time.h
@@ -1,7 +1,7 @@
 /*
- * ASN.1 time
+ * ASN.1 Time
  *
- * Copyright (c) 2016-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ * Copyright (c) 2016-2024 Alexei A. Smekalkine <ikle@ikle.ru>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
